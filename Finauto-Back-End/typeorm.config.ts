143a103f6f931e4ee7ec6e25013beccf32e56ee6
eviluster <eviluster87@gmail.com>
2025-03-30 16:24:00 -0400

    more update

--- Finauto-Back-End/typeorm.config.ts
+++ Finauto-Back-End/typeorm.config.ts
@@ -10,7 +10,7 @@ export default new DataSource({
   type: 'postgres',
   host: process.env.DB_HOST,
   port: parseInt(process.env.DB_PORT),
-  username: 'root',
+  username: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
   entities: ['src/**/entities/*.entity.ts'],
