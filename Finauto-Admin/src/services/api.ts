d24009e8b37100fc4f188fc4a57287f7aa6fc78c
eviluster <eviluster87@gmail.com>
2025-03-31 03:59:59 -0400

    uodate configs

--- Finauto-Admin/src/services/api.ts
+++ Finauto-Admin/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://127.0.0.1:5000/v1",
+  baseURL: "https://api.finautointernational.com/v1",
   timeout: 10000,
   headers: {
     "Content-Type": "application/json",
--- Finauto-Cliente/src/plugin/axios.ts
+++ Finauto-Cliente/src/plugin/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'https://api.example.com', // Cambia esto a tu URL base
+  baseURL: 'https://api.finautointernational.com/v1', // Cambia esto a tu URL base
   timeout: 5000, // Tiempo límite de solicitud
   headers: {
     'Content-Type': 'application/json',
