6cd41f184e3201bfbba642d7f15546f309513982
eviluster <eviluster87@gmail.com>
2025-03-31 13:38:43 -0400

    cambios en vite para acceder desde internet

--- Finauto-Admin/vite.config.mts
+++ Finauto-Admin/vite.config.mts
@@ -18,6 +18,7 @@ export default defineConfig({
   },
 
   // para publicar y se acceda por cualquier ip
+  base: '/',
   server: {
     host: '0.0.0.0',
     port: 8081
--- Finauto-Cliente/vite.config.js
+++ Finauto-Cliente/vite.config.js
@@ -5,6 +5,7 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   plugins: [vue()],
   // para publicar y escuche por cualquier ip
+  base: '/',
   server: {
     host: '0.0.0.0',
     port: 8080
