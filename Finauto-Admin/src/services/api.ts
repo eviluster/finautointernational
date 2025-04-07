dccc87b072ea937f9e9da1270fc566158bb666aa
eviluster <eviluster87@gmail.com>
2025-04-06 23:33:49 -0400

    `Update API URLs to use http://82.163.73.169:5000/v1 instead of https://api.finautointernational.com in various files`

--- Finauto-Admin/src/services/api.ts
+++ Finauto-Admin/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || 'http://82.163.73.169:5000/v1',
+  baseURL: import.meta.env.VITE_API_BASE_URL,
   timeout: 10000,
   headers: {
     "Content-Type": "application/json",
--- Finauto-Cliente/src/plugin/axios.ts
+++ Finauto-Cliente/src/plugin/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || 'http://82.163.73.169:5000/v1', // Cambia esto a tu URL base
+  baseURL: import.meta.env.VITE_API_BASE_URL, // Cambia esto a tu URL base
   timeout: 5000, // Tiempo límite de solicitud
   headers: {
     'Content-Type': 'application/json',
