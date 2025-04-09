173b13bbe09a6bb8271beba8674bc2eed65e7b81
eviluster <eviluster87@gmail.com>
2025-04-08 21:54:15 -0400

    Corrige módulo de pago y actualiza pago.service.ts

--- Finauto-Back-End/src/pago/pago.controller.ts
+++ Finauto-Back-End/src/pago/pago.controller.ts
@@ -2,20 +2,33 @@ import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/commo
 import { PagoService } from './pago.service';
 import { CreatePagoDto } from './dto/create-pago.dto';
 import { UpdatePagoDto } from './dto/update-pago.dto';
-import { BaseControllerCRUD } from 'src/common/base/class/base.controller.crud.class';
 
 @Controller('pago')
-export class PagoController extends BaseControllerCRUD<
-CreatePagoDto,
-UpdatePagoDto,
-PagoService
-> {
-  constructor(private readonly Service: PagoService) {
-    super(Service);
+export class PagoController {
+  constructor(private readonly pagoService: PagoService) {}
+
+  @Post()
+  create(@Body() createPagoDto: CreatePagoDto) {
+    return this.pagoService.create(createPagoDto);
   }
-  
-  // @Get('search-by-field')
-  // async searchByField(@Body() searchDto: SearchEngineDto): Promise<ReturnDto> {
-  //   return await this.Service.searchByField(searchDto);
-  // }
+
+  @Get()
+  findAll() {
+    return this.pagoService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.pagoService.findOne(+id);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updatePagoDto: UpdatePagoDto) {
+    return this.pagoService.update(+id, updatePagoDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.pagoService.remove(+id);
+  }
 }
--- Finauto-Back-End/src/pago/pago.service.ts
+++ Finauto-Back-End/src/pago/pago.service.ts
@@ -1,80 +1,26 @@
 import { Injectable } from '@nestjs/common';
 import { CreatePagoDto } from './dto/create-pago.dto';
 import { UpdatePagoDto } from './dto/update-pago.dto';
-import { BaseServiceCRUD } from 'src/common/base/class/base.service.crud.class';
-import { Pago } from './entities/pago.entity';
-import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
-import { ReturnDto } from 'src/common/base/dto';
-import { Configuracion } from 'src/configuracion/entities/configuracion.entity';
 
 @Injectable()
-export class PagoService   extends BaseServiceCRUD<
-Pago,
-CreatePagoDto,
-UpdatePagoDto> {
-  constructor(
-    @InjectRepository(Pago)
-    private readonly repository: Repository<Pago>,
-    @InjectRepository(Configuracion)
-    private readonly repositoryConfiguracion: Repository<Configuracion>,
-  ) {
-    super(repository)
+export class PagoService {
+  create(createPagoDto: CreatePagoDto) {
+    return 'This action adds a new pago';
   }
 
-  async getConfigPago(): Promise<Configuracion> {
-    const config = await this.repositoryConfiguracion.findOne({ where: { id: 'configPago' } });
-    return config;
+  findAll() {
+    return `This action returns all pago`;
   }
-  override async create(createDto: CreatePagoDto): Promise<ReturnDto> {
 
-    // 1 obtener el config % de pago para inicio prod
-    const config = await this.getConfigPago();
-
-    // obtengo la reserva para saber si tiene mas pagos y cto es el tota
-    
-
-    const entity = super.create(createDto);
-    return entity;
+  findOne(id: number) {
+    return `This action returns a #${id} pago`;
   }
-  // async searchByField(searchDto: SearchEngineDto): Promise<ReturnDto> {
-  //   const returnDto = new ReturnDto();
 
-  //     const queryBuilder = this.repository.createQueryBuilder('engine');
-  //      if(searchDto.name != undefined && 
-  //         searchDto.name != null )
-  //     {
-  //       if(searchDto.name.es != undefined && 
-  //         searchDto.name.es != null && 
-  //         searchDto.name.es && searchDto.name.es.trim() !== '')
-  //       { 
-  //         queryBuilder.andWhere(`engine.name->>'es'  LIKE :name`, { name: `%${searchDto.name.es}%` });
-  //       }
-  //       if(searchDto.name.en != undefined && 
-  //         searchDto.name.en != null && 
-  //         searchDto.name.en && searchDto.name.en.trim() !== '')
+  update(id: number, updatePagoDto: UpdatePagoDto) {
+    return `This action updates a #${id} pago`;
+  }
 
-  //       {
-  //         queryBuilder.andWhere(`engine.name->>'en' LIKE :name`, { name: `%${searchDto.name.en}%` });
-  //       }
-  //     }
-  //     if(searchDto.description)
-  //     {
-  //       if(searchDto.description.es != undefined &&
-  //         searchDto.description.es != null &&
-  //         searchDto.description.es && searchDto.description.es.trim() !== '')
-  //       { 
-  //         queryBuilder.andWhere(`engine.description->>'es' LIKE :description`, { description: `%${searchDto.description.es}%` });
-  //       }
-  //       if(searchDto.description.en != undefined &&
-  //         searchDto.description.en != null &&
-  //         searchDto.description.en && searchDto.description.en.trim() !== '')
-  //       {
-  //         queryBuilder.andWhere(`engine.description->>'en' LIKE :description`, { description: `%${searchDto.description.en}%` });
-  //       }
-  //     }
-  //     returnDto.data = await queryBuilder.getMany();
-  
-  //   return returnDto;
-  // }
+  remove(id: number) {
+    return `This action removes a #${id} pago`;
+  }
 }
